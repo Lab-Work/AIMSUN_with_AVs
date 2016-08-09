15c3ba7b2436a807c154bec48426a4764cd63087
Yanning Li <emlynlyn@gmail.com>
2016-08-09 15:50:44 -0500

    Updated readme and cleaned the repository

---
+++ Simulation/aimsun_files/veh_types/run_all.bat
@@ -0,0 +1,4 @@
+cd "C:\Users\TrafficControl\Dropbox\RenAimsunSimulation\simulation\aimsun_files\veh_types\"
+"C:\Program Files\TSS-Transport Simulation Systems\Aimsun 8.0\aconsole.exe" -project "C:\Users\TrafficControl\Dropbox\RenAimsunSimulation\simulation\aimsun_files\veh_types\sim_sce0_seed2143.ang" -cmd execute -target 1377
+"C:\Program Files\TSS-Transport Simulation Systems\Aimsun 8.0\aconsole.exe" -project "C:\Users\TrafficControl\Dropbox\RenAimsunSimulation\simulation\aimsun_files\veh_types\sim_sce100_seed2143.ang" -cmd execute -target 1377
+pause
