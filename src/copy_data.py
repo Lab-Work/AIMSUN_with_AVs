4ad78a9fa36fd3da85210a12950297a65b506187
Yanning Li <emlynlyn@gmail.com>
2017-01-23 10:03:19 -0600

    organize

--- src/copy_data.py
+++ src/copy_data.py
@@ -17,13 +17,14 @@ mdl = ['1st', '2nd']
 
 for sce, seed in sce_seeds:
 
-	os.mkdir(video_dir + 'Video_sce{0}_seed{1}_data/'.format(sce, seed))
+	if not os.path.exists(video_dir + 'Video_sce{0}_seed{1}_data/'.format(sce, seed))
+		os.mkdir(video_dir + 'Video_sce{0}_seed{1}_data/'.format(sce, seed))
 
 	for run in range(0, 10):
 
 		for p in prop:
 			for m in mdl:
-				src = result_dir + 'Estimation_corrected__{0}/Estimation{1}_PR_{2}_Seed{3}_{4}.npy'.format(run, p, sce, seed, m)
+				src = result_dir + 'Estimation_corrected_{0}/Estimation{1}_PR_{2}_Seed{3}_{4}.npy'.format(run, p, sce, seed, m)
 
 				target = video_dir + 'Video_sce{0}_seed{1}_data/'.format(sce, seed) + 'Estimation{0}_PR_{1}_Seed{2}_{3}_{4}.npy'.format(p, sce, seed, m, run)
 		
@@ -32,7 +33,7 @@ for sce, seed in sce_seeds:
 
 		if run == 0:
 			# copy the true state
-			src_true = result_dir + 'Estimation_corrected__{0}/TrueDensity_PR_{1}_Seed{2}_1st.npy'.format(run, sce, seed)
+			src_true = result_dir + 'Estimation_corrected_{0}/TrueDensity_PR_{1}_Seed{2}_1st.npy'.format(run, sce, seed)
 			target_true = video_dir + 'Video_sce{0}_seed{1}_data/'.format(sce, seed) + 'TrueDensity_PR_{0}_Seed{1}.npy'.format(sce, seed)
 			shutil.copy(src_true, target_true)
 
